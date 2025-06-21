9c55b59df980ec37c91bf83163e8d4d0da303b74
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-06-20 22:04:58 -0400

    doc: ThumbnailDAO

--- contexts/drawing_list/services/ThumbnailDAO.ts
+++ contexts/drawing_list/services/ThumbnailDAO.ts
@@ -1,5 +1,29 @@
 export interface ThumbnailDAO {
+
+  /**
+   * Saves the data of the thumbnail in the database.
+   * `id` and `timeStamp` are used to identificate a specific thumbnail
+   * @param id
+   * @param timeStamp
+   * @param data  Base64 with the thumbnail
+   * @returns  The link generated to access it
+   */
   save( id:string, timeStamp:number, data:string ): Promise<string>
+
+  /**
+   * Uses `id` and `timeStamp` to recover its saved thumbnail data
+   * @param id
+   * @param timeStamp
+   * @returns  Base64 data of the thumbnail, if it was not saved yet: null
+   */
   get( id:string, timeStamp:number ): Promise<string|null>
+
+  /**
+   * Eliminates the saved thumbnail. After this action the link generated with `ThumbnailDAO.save` is invalid
+   * `id` and `timeStamp` are used to identificate a specific thumbnail
+   * @param id
+   * @param timeStamp
+   */
   delete( id:string, timeStamp:number ): Promise<void>
+
 }
