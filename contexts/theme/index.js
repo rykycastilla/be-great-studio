f92bc14593e99b472cf90c1552645c664d5ebac4
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-07-05 15:11:10 -0400

    fix: solving types bug on macOS. working with case insensitivity

--- contexts/theme/index.js
+++ contexts/theme/index.js
@@ -1,5 +1,5 @@
 export { default as ThemeProvider } from './ThemeProvider'
-export { useTheme } from './theme'
+export { useTheme } from './use_theme'
 
 /** @typedef { import( './ThemeContext' ).ThemeContext } ThemeContext */
 /** @typedef { import( './Theme' ).Theme } Theme */
