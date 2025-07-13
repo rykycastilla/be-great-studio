6b3c444d849112485530fd0ba822ede18cc9c341
Orestes Ricardo Castilla Escalona <rcastilla744@gmail.com>
2025-07-12 20:36:39 -0400

    feat: Support bgpx importing

--- views/App/components/AddPictureButton.jsx
+++ views/App/components/AddPictureButton.jsx
@@ -1,15 +1,77 @@
+import * as FileSystem from 'expo-file-system'
 import { BUTTON_DEBOUNCE_DELAY } from '@/constants'
 import { debounce } from '@/utils/debounce'
 import { genId } from '@/utils/gen_id'
+import { getDocumentAsync } from 'expo-document-picker'
 import { Ionicons } from '@expo/vector-icons'
 import { StyleSheet, TouchableOpacity } from 'react-native'
-import { useCallback } from 'react'
+import { useCallback, useState } from 'react'
+import { useDrawingList } from '@/contexts/drawing_list'
+import { useLongCallback } from '@/hooks/long_callback'
 import { useRouter } from 'expo-router'
 import { useTheme } from '@/contexts/theme'
 
 /**
+ * @import { ExpoIcon } from '@/types/ExpoIcon'
  * @import { ReactElement } from 'react'
  */
+
+const BASE64_PREFIX = 'data:image/bgpx;base64,'
+
+/**
+ * @returns { () => Promise<void> }
+ */
+function useBgpxPicker() {
+  const { importDrawing } = useDrawingList()
+  return useCallback( async() => {
+    const { assets } = await getDocumentAsync( { multiple:true } )
+    if( assets === null ) { return }
+    for( const bgpxFile of assets ) {
+      const { name, uri } = bgpxFile
+      if( !name.endsWith( '.bgpx' ) ) { continue }
+      const base64 = await FileSystem.readAsStringAsync(
+        uri, { encoding:FileSystem.EncodingType.Base64 },
+      )
+      await importDrawing( BASE64_PREFIX + base64 )
+    }
+  }, [ importDrawing ] )
+}
+
+/**
+ * @typedef { object } AddState
+ * @property { ExpoIcon } icon
+ * @property { () => void } handle
+ */
+
+/**
+ * @returns { [ AddState, () => void ] }
+ */
+function useAddButtonToggle() {
+
+  const [ isImporting, setIsImporting ] = useState( false )
+  const router = useRouter()
+  const handleImporting = useBgpxPicker()
+
+  const toggleState = useLongCallback( () => {
+    setIsImporting( ( isImporting ) => !isImporting )
+  } )
+
+  const handleCreation = useCallback( () => {
+    const id = genId()
+    router.push( `/drawing/${ id }` )
+  }, [ router ] )
+
+  /** @type { AddState } */ const creation = {
+    icon:'add', handle:handleCreation,
+  }
+
+  /** @type { AddState } */ const importing = {
+    icon:'download', handle:handleImporting,
+  }
+
+  return [ isImporting ? importing : creation, toggleState ]
+
+}
 
 /**
  * @returns { ReactElement }
@@ -17,18 +79,15 @@ import { useTheme } from '@/contexts/theme'
 const AddPictureButton = () => {
 
   const { colors } = useTheme()
-  const router = useRouter()
-
-  const handlePress = useCallback( () => {
-    const id = genId()
-    router.push( `/drawing/${ id }` )
-  }, [ router ] )
+  const [ state, toggleState ] = useAddButtonToggle()
+  const { icon, handle } = state
 
   return (
     <TouchableOpacity
       style={ [ styles.addPictureButton, { backgroundColor:colors.primary } ] }
-      onPress={ debounce( handlePress, BUTTON_DEBOUNCE_DELAY ) }>
-      <Ionicons name="add" size={ 28 } color="#FFFFFF" />
+      onPress={ debounce( handle, BUTTON_DEBOUNCE_DELAY ) }
+      onLongPress={ toggleState }>
+      <Ionicons name={ icon } size={ 28 } color="#FFFFFF" />
     </TouchableOpacity>
   )
 
